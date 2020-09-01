80a042f23b64335ce47085a3177178543f721261
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-09-01 20:37:48 +0200

    Correction cible pour bouton Ajout d'article et Ajout de commentaires.

--- index.php
+++ index.php
@@ -10,10 +10,11 @@
     <body>
         <h1>Blog Jean Forteroche</h1>
         <p>Dernières publications :</p>
- 
+		<button id="ajoutbillet" onclick="window.location.href='add_post.php';">Ajouter un billet</button>
+
 <?php
 
-// On récupère les 5 dernières publications par ordre décroissant
+// On récupère les 5 derniers billets
 $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
 
 while ($donnees = $req->fetch())
@@ -27,11 +28,12 @@ while ($donnees = $req->fetch())
     
     <p>
     <?php
-    // Afficher le contenu du billet
+    // On affiche le contenu du billet
     echo nl2br(htmlspecialchars($donnees['contenu']));
     ?>
-    <br />
-    <button><em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em></button>
+     <br />
+    <button><em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Voir les commentaires</a></em></button>
+    <button><em><a href="add_commentaires.php?billet=<?php echo $donnees['id']; ?>">Ajouter un commentaire</a></em></button>
     </p>
 </div>
 <?php
