8647a186f9f97c960e04f47431fb9d035271f9d5
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-09-01 22:50:58 +0200

    Mise à jour du fichier index.php pour amélioer layout.

--- index.php
+++ index.php
@@ -4,41 +4,58 @@
     <head>
         <meta charset="utf-8" />
         <title>Blog JF</title>
-	<link href="style.css" rel="stylesheet" /> 
+        <title>JF Blog | Accueil </title>
+    	<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    	<link href="style.css" rel="stylesheet" /> 
     </head>
         
     <body>
-        <h1>Blog Jean Forteroche</h1>
-        <p>Dernières publications :</p>
-		<button id="ajoutbillet" onclick="window.location.href='add_post.php';">Ajouter un billet</button>
-
-<?php
-
-// On récupère les 5 derniers billets
-$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-while ($donnees = $req->fetch())
-{
-?>
-<div class="news">
-    <h3>
-        <?php echo htmlspecialchars($donnees['titre']); ?>
-        <em>le <?php echo $donnees['date_creation_fr']; ?></em>
-    </h3>
-    
-    <p>
-    <?php
-    // On affiche le contenu du billet
-    echo nl2br(htmlspecialchars($donnees['contenu']));
-    ?>
-     <br />
-    <button><em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Voir les commentaires</a></em></button>
-    <button><em><a href="add_commentaires.php?billet=<?php echo $donnees['id']; ?>">Ajouter un commentaire</a></em></button>
-    </p>
-</div>
-<?php
-} // Fin de la boucle des billets
-$req->closeCursor();
-?>
+		<div class="container">
+				<!-- container - wraps whole page -->
+	    
+        		<!-- menu -->
+        		<?php include('sections/menu.php') ?>
+        		<!-- // menu -->
+        
+        		<!-- banner -->
+        		<?php include('sections/banner.php') ?>
+        		<!-- // banner -->
+		    <div class="content">
+				<h2>Dernières publications :</h2>
+				<button id="ajoutbillet" onclick="window.location.href='add_post.php';">Ajouter un billet</button>
+                <br>
+                 <br>
+                <?php
+                
+                // On récupère les 5 dernières publications par ordre décroissant
+                $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+                
+                while ($donnees = $req->fetch()){
+                ?>
+                
+                <h3>
+                 <?php echo htmlspecialchars($donnees['titre']); ?>
+                <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+                </h3>
+                 <br>
+				<?php
+				// Afficher le contenu du billet
+				echo nl2br(htmlspecialchars($donnees['contenu']));
+				?>
+				<br />
+				<button><em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Voir les commentaires</a></em></button>
+				<button><em><a href="add_commentaires.php?billet=<?php echo $donnees['id']; ?>">Ajouter un commentaire</a></em></button>
+				<br>
+				<br>
+                <br>
+            	<?php
+            	} // Fin de la boucle des billets
+            	$req->closeCursor();
+            	?>
+        </div>
+	</div>
 </body>
 </html>
