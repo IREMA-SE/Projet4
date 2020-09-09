78335d254913bf5bb4b9d91af1b9ac8803e8cf94
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-09-09 13:05:22 +0200

    Séparation de code: de frontend à commentsManager pour affichage des commentaires.

---
+++ model/commentsManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OpenClassrooms\Blog\Model;
+
+require_once("model/manager.php");
+
+class CommentManager extends Manager
+{
+    public function getComments($postId)
+    {
+        $db = $this->dbConnect();
+        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments->execute(array($postId));
+
+        return $comments;
+    }
+
+    
+}
