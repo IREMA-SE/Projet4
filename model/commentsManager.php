afb6cfdb5b0ff692c69b1c7aeb1e150e4962244e
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-09-09 13:12:36 +0200

    Ajout fonction d'ajout de commentaires.

--- model/commentsManager.php
+++ model/commentsManager.php
@@ -15,5 +15,12 @@ class CommentManager extends Manager
         return $comments;
     }
 
-    
+    public function postComment($postId, $author, $comment)
+    {
+        $db = $this->dbConnect();
+        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+        return $affectedLines;
+    }
 }
