fd3b75ade9f7e97e374ef7737704c6b046c80057
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-09-11 21:06:37 +0200

    Suppression des liens CSS et Bootrap.

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Blog JF</title>
-        <title>JF Blog | Accueil </title>
-    	<link rel="apple-touch-icon" sizes="180x180" href='images/apple-touch-icon.png'>
-        <link rel="icon" type="image/png" sizes="32x32" href='images/favicon-32x32.png'>
-        <link rel="icon" type="image/png" sizes="16x16" href='images/favicon-16x16.png'>
-    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    	<link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
+
 		<div class="container">
         		<!-- menu -->
         		<?php include('menu.php') ?>
@@ -57,6 +44,3 @@
                     <?php require('template.php'); ?>
             </div>
 	    </div>
-		
-</body>
-</html>
