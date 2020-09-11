6b803444124a922bea654122fdea47d784b7fea7
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-09-11 21:04:29 +0200

    Séparation de code: déplacement des liens css et bootrap dans template.php

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -2,9 +2,18 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title><?= $title ?></title>
-        <link href="public/css/style.css" rel="stylesheet" /> 
+        <title>Blog JF</title>
+        <title>JF Blog | Accueil </title>
+    	<link rel="apple-touch-icon" sizes="180x180" href='images/apple-touch-icon.png'>
+        <link rel="icon" type="image/png" sizes="32x32" href='images/favicon-32x32.png'>
+        <link rel="icon" type="image/png" sizes="16x16" href='images/favicon-16x16.png'>
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    	<link href="style.css" rel="stylesheet" /> 
     </head>
+        
+
+
+
         
     <body>
         <?= $content ?>
