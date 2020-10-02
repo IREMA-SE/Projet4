1fa1bc3ccbcf7bba384c1fb34b22bb1254742491
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-10-02 19:52:25 +0200

    Mise à jour: suppression de lines inutiles.

--- view/frontend/listCommentsView.php
+++ view/frontend/listCommentsView.php
@@ -1,45 +1,44 @@
-<?php $title = 'Mon blog'; ?>
-
 <?php ob_start(); ?>
-<h1>Comments Lists</h1>
-
-
-<?php
-
-while ($data = $posts->fetch())
-{
-?>
-    <div class="news">
-        <h3>
-            Post:<?= htmlspecialchars($data['title']) ?>
-        </h3>
-        <p>
-            Author: <?= htmlspecialchars($data['author']) ?>
-        <br>
+    <div class="container">
+        <h1>Liste des commentaires</h1>
         
-           Commnt: <?= nl2br(htmlspecialchars($data['comment'])) ?>
-            <br />
-           
-            <?php 
-			if($user['level'] == 1){
-				if($data['approved'] == 0){
-				?> 
-				<em><a href="index.php?action=approvecomment&act=1&amp;id=<?= $data['id'] ?>">Approve</a></em>
-				<?php 
-				}
-				
-				?> 
-				<em><a href="index.php?action=approvecomment&act=2&amp;id=<?= $data['id'] ?>">Delete</a></em>
-				<?php 
-				
-			}
-			?>
-        </p>
-    </div>
-<?php
-}
-$posts->closeCursor();
-?>
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
+        <?php
+        
+        while ($data = $posts->fetch())
+        {
+        ?>
+            <div class="news">
+                <h3>
+                    Post:<?= htmlspecialchars($data['title']) ?>
+                </h3>
+                <p>
+                    Author: <?= htmlspecialchars($data['author']) ?><br />
+                <br />
+                
+                   Commnt: <?= nl2br(htmlspecialchars($data['comment'])) ?>
+                    <br />
+                   
+                    <?php 
+        			if($user['level'] == 1){
+        				if($data['approved'] == 0){
+        				?> 
+        				<em><a href="index.php?action=approvecomment&act=1&amp;id=<?= $data['id'] ?>">Valider</a></em>
+        				<?php 
+        				}
+        				
+        				?> 
+        				<em><a href="index.php?action=approvecomment&act=2&amp;id=<?= $data['id'] ?>">Supprimer</a></em>
+        				<?php 
+        				
+            			}
+            			?>
+                    </p>
+                </div>
+        </div>
+    <?php
+    }
+    $posts->closeCursor();
+    $content = ob_get_clean();
+    ?>
+    <?php require('template.php'); ?>
+</div>
