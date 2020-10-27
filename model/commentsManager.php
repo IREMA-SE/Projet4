ae0d6d434d0a84c993f5f131caf121a1bc201939
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-10-27 21:27:21 +0100

    Mise à jour: Ajout du nom de l'autheur d'un commentaire via son ID dans la Table des utilisateurs.

--- model/commentsManager.php
+++ model/commentsManager.php
@@ -1,26 +1,52 @@
 <?php
 
-namespace OpenClassrooms\Blog\Model;
+//namespace OpenClassrooms\Blog\Model;
 
-require_once("model/manager.php");
+require_once("manager.php");
 
 class CommentManager extends Manager
 {
-    public function getComments($postId)
-    {
-        $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
-        $comments->execute(array($postId));
+	
+     public function getComments($postId)
+	{
+		$db = $this->dbConnect();
+		$comments = $db->prepare('SELECT id, author,  post_id, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\') AS comment_date_fr FROM comments WHERE post_id = ? AND approved = ? ORDER BY comment_date DESC');
+		$comments->execute(array($postId, 1));
 
-        return $comments;
-    }
+		return $comments;
+	}
 
-    public function postComment($postId, $author, $comment)
-    {
-        $db = $this->dbConnect();
-        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
-        $affectedLines = $comments->execute(array($postId, $author, $comment));
+	 public function getallcomments(){
+		$db = $this->dbConnect();
+		$comments = $db->prepare('SELECT comments.id, comments.author, comments.comment, comments.approved, posts.title FROM comments INNER JOIN posts ON posts.id = comments.post_id WHERE comments.approved = 0');
+		$comments->execute(array( ));
 
-        return $affectedLines;
-    }
+		return $comments;
+	}
+
+
+	 public function postComment($postId, $author, $comment)
+	{
+		$db = $this->dbConnect();
+		$comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+		$affectedLines = $comments->execute(array($postId, $author, $comment));
+
+		return $affectedLines;
+	}
+
+
+
+	 public function appovcomment($id, $act){
+		$db = $this->dbConnect();
+		
+		if($act == 2){
+			
+			$affectedLines = $db->query('DELETE FROM comments WHERE id = ' . $id);
+		}else if($act == 1 || $act == 0){
+			$affectedLines = $db->query('UPDATE comments SET approved = "'. $act . '" WHERE id = ' . $id);
+		}
+		
+		 
+		return $affectedLines;
+	}
 }
