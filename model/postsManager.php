1b3ad461ff9a04625a9fc34ddf02e00cf17d5e84
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-10-27 21:31:36 +0100

    Mise à jour ajout d'article.

--- model/postsManager.php
+++ model/postsManager.php
@@ -1,26 +1,51 @@
 <?php
 
-namespace OpenClassrooms\Blog\Model;
+//namespace OpenClassrooms\Blog\Model;
 
-require_once("model/manager.php");
+require_once("manager.php");
+
 
 class PostManager extends Manager
 {
     public function getPosts()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 10');
 
         return $req;
     }
 
+	public function delPost($postId)
+	{
+		$db = $this->dbConnect();
+		$affectedLines = $db->query('DELETE FROM posts WHERE id = ' . $postId);
+		   
+		return $affectedLines;
+	}
     public function getPost($postId)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts WHERE id = ?');
         $req->execute(array($postId));
         $post = $req->fetch();
 
         return $post;
     }
+	public function postBlog($id, $title, $content){
+		
+		$db = $this->dbConnect();
+		
+		if($id > 0){
+			
+			
+			$affectedLines = $db->query('UPDATE posts SET title = "'. $title . '", content = "'.$content.'" WHERE id = ' . $id);
+		   
+			return $affectedLines;
+		}else{
+			$comments = $db->prepare('INSERT INTO posts( title, content, creation_date) VALUES(?, ?, NOW())');
+			$affectedLines = $comments->execute(array($title, $content));
+	 
+			return $affectedLines;
+		}
+	}
 }
