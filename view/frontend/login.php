04720d81287510d4938222dd15d184a612757150
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-10-28 14:08:13 +0100

    Mise à jour fonction de login.

--- view/frontend/login.php
+++ view/frontend/login.php
@@ -1,21 +1,18 @@
-<?php ob_start(); ?>
 
-<div class="container">
-    <h2>Se connecter</h2>
-    <form action="index.php?action=login" method="post">
-    	
-        <div>
-            <label for="email">Adresse E-mail</label><br />
-            <input type="text" id="email"  name="email" />
-        </div>
-        <div>
-            <label for="password">Mot de passe</label><br />
-            <input id="password" type="password" name="password" />
-        </div>
-        <div>
-            <input type="submit" />
-        </div>
-    </form>
-</div>
+<?php ob_start(); ?>
+       <h4>Se connecter</h4>
+        <form action="index.php?action=login" method="post">
+            <div>
+                <label for="email">Adresse E-mail</label><br />
+                <input type="text" id="email"  name="email" />
+            </div>
+            <div>
+                <label for="password">Mot de passe</label><br />
+                <input id="password" type="password" name="password" />
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
+          </form>
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
