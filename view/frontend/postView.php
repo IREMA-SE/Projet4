2b57e24f7a051cd6ce733aa5621c585f989e249c
Emmanuel Coulibaly <nieyake@yahoo.fr>
2020-10-28 14:12:15 +0100

    Mise à jour page d'article.

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,51 +1,53 @@
 <?php $title = htmlspecialchars($post['title']); ?>
 
 <?php ob_start(); ?>
-<h1>Mon super blog !</h1>
-<p><a href="index.php">Retour à la liste des billets</a></p>
 
-<div class="news">
-    <h3>
-        <?= htmlspecialchars($post['title']) ?>
-        <em>le <?= $post['creation_date_fr'] ?></em>
-    </h3>
-    
-    <p>
-        <?= nl2br(htmlspecialchars($post['content'])) ?>
-    </p>
-</div>
-
-<h2>Commentaires</h2>
-<?php
-	if($isauth == 1){
-?>
-<form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-    <div>
-        <label for="author">Auteur</label><br />
-        <input type="text" id="author" name="author" />
-    </div>
-    <div>
-        <label for="comment">Commentaire</label><br />
-        <textarea id="comment" name="comment"></textarea>
-    </div>
-    <div>
-        <input type="submit" />
-    </div>
-</form>
-
-<?php
-	}
-	
-while ($comment = $comments->fetch())
-{
-?>
-    <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-    <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-    <a href="index.php?action=post&id=<?php echo $comment['post_id']; ?>&disp=<?php echo $comment['id']; ?>">Dispute</a>
-    <hr />
-<?php
-}
-?>
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
+		<button> <a  href="index.php">Retour en accueil</a></button>
+            <h4>
+                <?= htmlspecialchars($post['title']) ?><br />
+                <span class="createdate"><em>le <?= $post['creation_date_fr'] ?></em></span> 
+            </h4>
+            
+            <p>
+                <?= $post['content'] ?>
+            </p>
+       
+        <br>
+        <h4>Commentaires des lecteurs</h4>
+        <br>
+        <?php
+        	if($isauth == 1){
+        ?>
+        <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+           
+            <div>
+                <label for="comment">Ajouter un commentaire</label><br />
+                <textarea id="txtarea" name="comment"></textarea>
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
+        
+        <?php
+        	}
+        	
+        while ($comment = $comments->fetch())
+        {
+        ?>
+            <p><strong><?= htmlspecialchars($comment['author']) ?></strong><br> 
+            <span class="createdate">Posté le: <?= $comment['comment_date_fr'] ?></span><br>
+            <p><?= $comment['comment'] ?></p>
+            <?php
+			if(isset($_SESSION["logged"]) && $_SESSION["logged"] == 1){
+			?>
+            <a href="index.php?action=post&id=<?php echo $comment['post_id']; ?>&disp=<?php echo $comment['id']; ?>">Signaler ce commentaire</a>
+            <?php
+			}
+			?>
+            <hr />
+        <?php
+        }
+        ?>
+    <?php $content = ob_get_clean(); ?>
+    <?php require('template.php'); ?>
